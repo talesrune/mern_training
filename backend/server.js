ee5999467497f9b810fc2f87e21d1c8afa39c0d0
Ting Wei <ltwei96@gmail.com>
2022-07-18 20:08:00 +0800

    initial setup for backend

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+
+// app.use(()=>{ //use is get/put/post
+//     console.log("We got a request")
+// })
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(4000,() =>{
+    console.log("listening on port 4000")
+})
