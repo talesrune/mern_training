daca6245b1719151c4c0eb6bb12d0612a6577e10
Ting Wei <ltwei96@gmail.com>
2022-07-19 12:19:30 +0800

    Added login page html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <Login />
     </div>
   );
 }
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,19 @@
+const Login = () => {
+  return (
+    
+    <div className="container">
+
+      <form>
+        <label for="email">Email:</label>
+        <input type="text" id = "email" name="email" placeholder="Please enter your email"></input>
+        <tr></tr>
+        <label for="pwd">Password:</label>
+        <input type="text" id = "pwd" name="pwd" placeholder="Please enter your password"></input>
+        <input type="submit" className="btn" value="Login"></input>
+      </form>
+    </div>
+    
+  )
+}
+
+export default Login
